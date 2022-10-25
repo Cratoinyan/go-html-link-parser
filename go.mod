794069d129f2f296b4eec086e18c41a228a8d4c8
Cratoinyan <umutciloglu06@gmail.com>
2022-10-25 10:46:59 +0300

    define the basic structure of the parse and implement basic dfs to parse the tree

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gophercises/link
+
+go 1.13
+
+require golang.org/x/net v0.1.0 // indirect
---
+++ parse.go
@@ -0,0 +1,32 @@
+package link
+
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+//Link node in an html doc eg(<a href="...">)
+type Link struct {
+	Href string
+	Text string
+}
+
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	dfs(doc, "")
+
+	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"	")
+	}
+}
